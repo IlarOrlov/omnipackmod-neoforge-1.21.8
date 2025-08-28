a1b2dcf5ab4d53dddb4ea4d86b4d30e6f7a66cd3
Errant Wanderer <ilar.foremost@gmail.com>
2025-08-28 12:42:21 +0300

    Initial commit

---
+++ src/main/java/net/errantwanderer/omnipackmod/OmniPackClient.java
@@ -0,0 +1,31 @@
+package net.errantwanderer.omnipackmod;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = OmniPackMod.MOD_ID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = OmniPackMod.MOD_ID, value = Dist.CLIENT)
+public class OmniPackClient {
+    public OmniPackClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        OmniPackMod.LOGGER.info("HELLO FROM CLIENT SETUP");
+        OmniPackMod.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
